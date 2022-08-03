38717ca4f22248188421d4e951d569d0ac6369e7
Rick Ansay <rick.ansay@gmail.com>
2022-08-02 22:31:01 -0600

    Add note

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,8 @@
 import Web3 from "web3";
  
 window.ethereum.request({ method: "eth_requestAccounts" });
- 
+
+// Injects the metamask web3 service
 const web3 = new Web3(window.ethereum);
  
 export default web3;
